ebe07940032a20b6f40f5ab94fce2437ea9a6046
Ufuk Bozdemir <ufb007@gmail.com>
2023-12-14 20:15:57 +0000

    Middleware created for api token authorization

---
+++ app/Http/Middleware/AuthApiToken.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //Make sure bearer token is set in the header and that it matches the string within the environment file
+        if (!$request->header('Authorization') || !Str::contains($request->header('Authorization'), env('API_TOKEN'))) {
+            return response()->json(['Error' => 'Authorisation missing'], 401);
+        }
+
+        return $next($request);
+    }
+}
